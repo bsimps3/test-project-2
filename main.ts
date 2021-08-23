ebb872660d715237202ba9715bf5ad909070f856
Brian Simpson <bsimps3@gmail.com>
2021-08-23 14:53:36 -0500

    rough draft of a start

--- main.ts
+++ main.ts
@@ -1,3 +1,24 @@
+control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MICROBIT_BUTTON_EVT_DOWN, function () {
+    music.playMelody("C D E F G A B C5 ", 120)
+})
+soundExpression.happy.play()
+basic.showString("")
+soundExpression.giggle.play()
+basic.showString("")
+let myImage = images.createBigImage(`
+    . . . . . . . . . .
+    . # # # . . # # # .
+    . # . # . . # . # .
+    . # # # . . # # # .
+    # . . . # . # . # .
+    `)
+while (true) {
+    while (input.buttonIsPressed(Button.A)) {
+    	
+    }
+    myImage.showImage(0, 400)
+    myImage.showImage(5, 400)
+}
 basic.forever(function () {
 	
 })
